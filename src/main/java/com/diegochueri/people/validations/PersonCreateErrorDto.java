54e3250febfbf06b83cbfc8af82fa39145690532
diego <diegochueri@gmail.com>
2023-01-04 15:07:56 -0400

    feat: added validation error handler

---
+++ src/main/java/com/diegochueri/people/validations/PersonCreateErrorDto.java
@@ -0,0 +1,19 @@
+package com.diegochueri.people.validations;
+
+public class PersonCreateErrorDto {
+	private String field;
+	private String error;
+
+	public PersonCreateErrorDto(String field, String error) {
+		this.field = field;
+		this.error = error;
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	public String getError() {
+		return error;
+	}
+}
