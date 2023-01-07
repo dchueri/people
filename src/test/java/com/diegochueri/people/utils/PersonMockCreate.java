a7d3a57706fd5bda4e773a8ddcefd9eb7b1f6196
diego <diegochueri@gmail.com>
2023-01-07 14:15:37 -0400

    test: added more tests to PersonController

--- src/test/java/com/diegochueri/people/utils/PersonMockCreate.java
+++ src/test/java/com/diegochueri/people/utils/PersonMockCreate.java
@@ -4,12 +4,15 @@ import java.time.LocalDate;
 
 import com.diegochueri.people.controllers.dto.PersonCreateDto;
 import com.diegochueri.people.controllers.dto.PersonDto;
+import com.diegochueri.people.controllers.dto.PersonUpdateDto;
 import com.diegochueri.people.models.Person;
 
 public class PersonMockCreate {
 	Long id = (long) 1;
 	String name = "Person";
+	String nameUpdated = "Person Updated";
 	LocalDate birthDate = LocalDate.parse("1995-03-16");
+	LocalDate birthDateUpdated = LocalDate.parse("1997-12-15");
 
 	public Person personAdd() {
 		Person person = new Person();
@@ -30,5 +33,12 @@ public class PersonMockCreate {
 		personCreate.setName(name);
 		personCreate.setBirthDate(birthDate);
 		return personCreate;
+	}
+
+	public PersonUpdateDto personUpdateDtoAdd() {
+		PersonUpdateDto personUpdate = new PersonUpdateDto();
+		personUpdate.setName(nameUpdated);
+		personUpdate.setBirthDate(birthDateUpdated);
+		return personUpdate;
 	}
 }
